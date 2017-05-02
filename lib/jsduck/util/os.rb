be2c427e8af93bc4002a3ce1cd2cffad21ab2ef7
Fabrício Zimmerer Murta <fabricio@object.net>
2017-05-02 17:03:53 -0300

    Sets cygwin environment as a linux-compatible environment.

--- lib/jsduck/util/os.rb
+++ lib/jsduck/util/os.rb
@@ -6,7 +6,7 @@ module JsDuck
     # Simple helper class to detect operating system
     class OS
       def self.windows?
-        RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
+        RbConfig::CONFIG['host_os'] =~ /mswin|mingw/
       end
     end
 
