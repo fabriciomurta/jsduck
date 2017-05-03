d66043c8dd19d4e744adec3368343e52847e9c9b
Fabrício Zimmerer Murta <fabricio@object.net>
2017-05-03 00:11:18 -0300

    Suppresses class's icons output to styles.css.
    The output CSS class was using file resources outside default
    resources/images/icons.png.

--- lib/jsduck/web/css.rb
+++ lib/jsduck/web/css.rb
@@ -15,11 +15,21 @@ module JsDuck
       end
 
       def write(filename)
-        File.open(filename, 'w') {|f| f.write(all_css) }
+        #File.open(filename, 'w') {|f| f.write(all_css) }
+        File.open(filename, 'w') {|f| f.write(noicon_css) }
         Util::MD5.rename(filename)
       end
 
       private
+
+      # icon CSS lines are overlapping with resources/images/icons
+      # usage by extjs's docs/resources/css/app.css.
+      def noicon_css
+        [
+          css_from_tags,
+          @opts.css,
+        ].join
+      end
 
       def all_css
         [
